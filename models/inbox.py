c8173392518231793103c05a29252ec89a7a2f1a
Ayham Alali <i.ayhamalali@gmail.com>
2026-02-19 14:36:36 +0300

    Update!

--- models/inbox.py
+++ models/inbox.py
@@ -2,7 +2,7 @@
 Unified message inbox and outbox management
 """
 
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import Optional, List, Any
 
 from db_helper import get_db, execute_sql, fetch_all, fetch_one, commit_db, DB_TYPE
