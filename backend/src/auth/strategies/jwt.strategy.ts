3c7841889b2a0d93f9d2583d0cfe792b7c77bf5a
Ayham Alali <i.ayhamalali@gmail.com>
2026-04-20 13:59:44 +0300

    Update!

--- backend/src/auth/strategies/jwt.strategy.ts
+++ backend/src/auth/strategies/jwt.strategy.ts
@@ -1,20 +1,18 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
-import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly configService: ConfigService) {
+  constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: configService.get<string>('JWT_SECRET') || 'defaultSecretChangeThisInProduction',
+      secretOrKey: process.env.JWT_SECRET || 'defaultSecretChangeThisInProduction',
     });
   }
 
   async validate(payload: { sub: string; username: string; status: string }) {
-    // This payload will be injected into request.user via our @CurrentUser() decorator
     return { id: payload.sub, username: payload.username, status: payload.status };
   }
 }
