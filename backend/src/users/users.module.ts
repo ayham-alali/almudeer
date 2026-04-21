98bfd9223c470f404ac8115c1ababd73f74edddf
Ayham Alali <i.ayhamalali@gmail.com>
2026-04-21 14:55:04 +0300

    Update!

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,7 +1,9 @@
 import { Module, Logger } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   providers: [UsersService],
   exports: [UsersService],
 })
